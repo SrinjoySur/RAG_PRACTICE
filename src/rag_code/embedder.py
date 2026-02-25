a9877411b7a9ffbb4ca4951d315daa5c9147f7b7
Srinjoy Sur <srinjoy_sur@epam.com>
2026-02-25 10:44:23 +0530

    feat: implemented local file embedding and updated dependencies

--- src/rag_code/embedder.py
+++ src/rag_code/embedder.py
@@ -1,12 +1,11 @@
 import os
+from abc import ABC,abstractmethod
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_openai.embeddings import AzureOpenAIEmbeddings
-#from langchain.embeddings import AzureOpenAIEmbeddings
-from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.document_loaders import WebBaseLoader,UnstructuredFileLoader
 from langchain_core.documents import Document
-from typing import Optional,List
+from typing import Optional,List,Any
 from dotenv import load_dotenv,find_dotenv
-import psycopg2
 import logging
 from pathlib import Path
 from datetime import datetime
@@ -25,7 +24,24 @@ logger = logging.getLogger(__name__)
 
 _=load_dotenv()
 
-class HFEmbedder:
+class Embedder(ABC):
+    def get_web_list_document(self,web_path:Optional[str]=None)->List[Document]:
+        document= WebBaseLoader(web_path=web_path or "https://www.espn.com/").load()
+        return document
+    def get_local_list_document(self,file_path:str,mode:Optional[str]="elements",strategy:Optional[str]="fast")->List[Document]:
+        document=UnstructuredFileLoader(file_path=file_path,mode=mode or "elements", strategy=strategy)
+        return document.load()
+    def get_list_document_contents(self,document:List[Document])->List[str]:
+        pages=[]
+        for text in document:
+            pages.append(text.page_content)
+        return pages
+    def print_doc_embeddings(self,embeddings):
+        print(embeddings)
+    @abstractmethod
+    def get_embeddings_of_document_contents(self,pages:List[str])->List[List[float]]:
+        pass
+class HFEmbedder(Embedder):
     def __init__(self,api_key:Optional[str]=None,model_name:Optional[str]="BAAI/bge-m3") -> None:
         self.api_key=api_key or os.getenv("HF_API_KEY","<YOUR_HF_API_KEY>")
         self.model_name=model_name
@@ -41,25 +57,22 @@ class HFEmbedder:
         except Exception as e:
             logger.error(f"[ERROR] HF Embedder initialization has encountered exception:{e}")
             self.client=None
-    def get_web_list_document(self,web_path:Optional[str]=None)->List[Document]:
-        document= WebBaseLoader(web_path=web_path or "https://www.espn.com/").load()
-        return document
-    def get_list_document_contents(self,document:List[Document])->List[str]:
-        pages=[]
-        for text in document:
-            pages.append(text.page_content)
-        return pages
-    def get_embeddings_of_document_contents(self,pages:List[str]):
+    def get_embeddings_of_document_contents(self,pages:List[str])->List[List[float]]:
+        """
+        Returns embeddings for a list of document pages using the Hugging Face
+        client. Logs an error if the client is not initialized or if an
+        exception occurs. Returns a list of embeddings or None on failure.
+        """
         try:
             if not self.client:
                 logger.error("[ERROR]  HF Client not Initialized")
+                return [[]]
             else:
                 return self.client.embed_documents(pages)
         except Exception as e:
             logger.error(f"[ERROR] HF Embeddings get_embeddings_of_document_contents() has encountered an exception:{e}")
-    def print_doc_embeddings(self,embeddings):
-        print(embeddings)
-class DialEmbedder:
+            return [[]]
+class DialEmbedder(Embedder):
     def __init__(self,api_key:Optional[str]=None,model_name:Optional[str]="text-embedding-3-small-1") -> None:
         self.api_key=api_key or os.getenv("DIAL_API_KEY","<YOUR_DIAL_API_KEY>")
         self.model_name=model_name
@@ -82,24 +95,16 @@ class DialEmbedder:
         except Exception as e:
             logger.error(f"[ERROR] Error the Dial CLient Could not be initialized due to exception:{e}")
             self.client=None
-    def get_web_list_document(self,web_path:Optional[str]=None)->List[Document]:
-        document= WebBaseLoader(web_path=web_path or "https://www.espn.com/").load()
-        return document
-    def get_list_document_contents(self,document:List[Document])->List[str]:
-        pages=[]
-        for text in document:
-            pages.append(text.page_content)
-        return pages
-    def get_embeddings_of_document_contents(self,pages:List[str]):
+    def get_embeddings_of_document_contents(self,pages:List[str])->List[List[float]]:
         try:
             if not self.client:
                 logger.error("Azure Embbeder Client Not Instantiated.")
+                return [[]]
             else:
                 return self.client.embed_documents(pages)
         except Exception as e:
             logger.error(f"[ERROR] Exception occured at Azure Embedding get_embeddings_of_document_contents:{e}")
-    def print_doc_embeddings(self,embeddings):
-        print(embeddings)
+            return [[]]
 def run_embedding_process(embedder:Optional[str]="HF"):
     aembedder=vars
     if embedder=="HF":
@@ -108,9 +113,11 @@ def run_embedding_process(embedder:Optional[str]="HF"):
         aembedder=DialEmbedder()
     if not aembedder.client:
         logger.error("[ERROR] Emmbeder Client could not be initialized correctly")
-    document=aembedder.get_web_list_document("https://jayant017.medium.com/rag-using-langchain-part-5-hypothetical-document-embeddings-hyde-050f57dfc252")
+    #document=aembedder.get_web_list_document("https://jayant017.medium.com/rag-using-langchain-part-5-hypothetical-document-embeddings-hyde-050f57dfc252")
+    document=aembedder.get_local_list_document("src/data/rag_sample_qas_from_kis.csv")
     pages=aembedder.get_list_document_contents(document)
     embeddings=aembedder.get_embeddings_of_document_contents(pages)
+    logger.info(f"[OK] Response Recieved:{embeddings}")
     aembedder.print_doc_embeddings(embeddings)
 if __name__=="__main__":
     run_embedding_process()
